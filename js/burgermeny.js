a372d36337c8b9c64db949d09a46844ee1f9c9a6
P.F.Halle <peha0008@stud.ek.dk>
2026-01-06 17:22:50 +0100

    comit med endringer

--- js/burgermeny.js
+++ js/burgermeny.js
@@ -1,3 +1,5 @@
+// BURGERMENY
+
 const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
@@ -6,3 +8,27 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+
+// DARK MODE
+const toggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    toggleBtn.innerText = "Light Mode";
+}
+
+toggleBtn.addEventListener('click', () => {
+    let theme = 'light';
+    if (document.documentElement.getAttribute('data-theme') !== 'dark') {
+        theme = 'dark';
+        toggleBtn.innerText = "Light Mode";
+    } else {
+        toggleBtn.innerText = "Dark Mode";
+    }
+    
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme); // Husker valget til neste gang
+});
+
